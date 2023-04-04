5d9eb5f440bfd94b4e0ea4fdb4dfaa551d0d1536
saro24 <saroarlene@gmail.com>
2023-04-04 21:08:02 +0200

    add six nbrs

---
+++ 0x00-JS_Basics/Numbers/0x01-six_together.js
@@ -0,0 +1,11 @@
+let num1 = 2;
+let num2 = 4;
+let num3 = 6;
+let num4 = 8;
+let num5 = 10;
+let num6 = 12;
+
+let sum = num1 + num2 + num3 + num4 + num5 + num6;
+
+console.log(sum); // Output: 42
+
